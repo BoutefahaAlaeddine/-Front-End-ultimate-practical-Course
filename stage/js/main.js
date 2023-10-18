a4473a49d8a561ec407c1b6b3c0c6b134c0bd6a2
boutefahaalaeddine <boutefahaalaeddine@gmail.com>
2023-10-18 03:07:21 +0100

    Lesson 12,13,14

--- stage/js/main.js
+++ stage/js/main.js
@@ -1,6 +1,14 @@
 $(function () {
   "use strict";
+
+  //toggle sidebar
   $(".toggle-sidebar").on("click", function () {
     $(".content-area , .sidebar").toggleClass("no-sidebar");
+  });
+
+  //toggle submenu
+  $(".toggle-submenu").on("click", function () {
+    $(this).find(".fa-angle-right").toggleClass("down");
+    $(this).next(".child-links").slideToggle();
   });
 });
