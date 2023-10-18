24d4d5fd5c5a04522576729219310662322b7f14
boutefahaalaeddine <boutefahaalaeddine@gmail.com>
2023-10-18 21:07:08 +0100

    lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,3 +12,44 @@ $(function () {
     $(this).next(".child-links").slideToggle();
   });
 });
+
+//Open / Close FullScreen
+$(".toggle-fullscreen").on("click", function () {
+  $(this).toggleClass("full-screen");
+  if ($(this).hasClass("full-screen")) {
+    openFullscreen();
+  } else {
+    closeFullscreen();
+  }
+});
+
+var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.WebkitRequestFullScreen) {
+    /* chrome, Safari and Opera */
+    elem.webkitRequestFullScreen();
+  } else if (elem.msRequestFullScreen) {
+    /* IE/Edge */
+    elem.msRequestFullScreen();
+  }
+}
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.WebkitExitFullScreen) {
+    /* chrome, Safari and Opera */
+    document.WebkitExitFullScreen();
+  } else if (document.msExitFullScreen) {
+    /* IE/Edge */
+    document.msExitFullScreen();
+  }
+}
