46467fa09acf1bd3e55c1dd2b4967809bd9f0423
boutefahaalaeddine <boutefahaalaeddine@gmail.com>
2023-10-20 01:06:39 +0100

    lesson 27

--- stage/js/main.js
+++ stage/js/main.js
@@ -39,6 +39,16 @@ $(function () {
       .removeClass(themesClasses.join(" "))
       .addClass($(this).data("theme"));
   });
+  //switch Font options
+  var fontClasses = [];
+  $(".font-options select option").each(function () {
+    fontClasses.push($(this).val());
+  });
+  $(".font-options select").on("change", function () {
+    $("body")
+      .removeClass(fontClasses.join(" "))
+      .addClass($(this).find("option:selected").val());
+  });
   //
 });
 
